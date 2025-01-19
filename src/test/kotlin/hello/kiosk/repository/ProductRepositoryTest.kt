32c5045b8ce48834a564b063ab7267705da64113
seop <useop0821@gmail.com>
2025-01-20 08:07:02 +0900

    test: 상품번호를 001 -> A-001으로 변경

--- src/test/kotlin/hello/kiosk/repository/ProductRepositoryTest.kt
+++ src/test/kotlin/hello/kiosk/repository/ProductRepositoryTest.kt
@@ -7,7 +7,6 @@ import hello.kiosk.domain.product.ProductSellingStatus.SOLD_OUT
 import hello.kiosk.domain.product.ProductType
 import hello.kiosk.domain.product.ProductType.*
 import jakarta.transaction.Transactional
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -24,9 +23,9 @@ class ProductRepositoryTest @Autowired constructor(
     @Test
     fun findAllBySellingStatusIn() {
         //given
-        val product1 = createProduct("001", COFFEE, SELLING, "아메리카노", 3000)
-        val product2 = createProduct("002", BEVERAGE, SELLING, "카페라떼", 5000)
-        val product3 = createProduct("003", DESSERT, SOLD_OUT, "치즈케이크", 7000)
+        val product1 = createProduct("A-001", COFFEE, SELLING, "아메리카노", 3000)
+        val product2 = createProduct("B-001", BOTTLE, SELLING, "카페라떼", 5000)
+        val product3 = createProduct("C-001", DESSERT, SOLD_OUT, "치즈케이크", 7000)
         productRepository.saveAll(listOf(product1, product2, product3))
 
         //when
@@ -46,49 +45,61 @@ class ProductRepositoryTest @Autowired constructor(
     @Test
     fun findAllByProductNumberIn() {
         //given
-        val product1 = createProduct("001", COFFEE, SELLING, "아메리카노", 3000)
-        val product2 = createProduct("002", BEVERAGE, SELLING, "카페라떼", 5000)
-        val product3 = createProduct("003", DESSERT, SOLD_OUT, "치즈케이크", 7000)
-        productRepository.saveAll(listOf(product1, product2, product3))
+        val product1 = createProduct("A-001", COFFEE, SELLING, "아메리카노", 3000)
+        val product2 = createProduct("A-002", BOTTLE, SELLING, "카페라떼", 5000)
+        val product3 = createProduct("A-003", DESSERT, SOLD_OUT, "치즈케이크", 7000)
+        val product4 = createProduct("B-001", BOTTLE, SOLD_OUT, "핫초코", 5000)
+        productRepository.saveAll(listOf(product1, product2, product3, product4))
 
         //when
-        val products = productRepository.findAllByProductNumberIn(listOf("001", "002"))
+        val products = productRepository.findAllByProductNumberIn(listOf("A-001", "A-002"))
 
         //then
         assertThat(products).hasSize(2)
             .extracting("productNumber", "name", "sellingStatus")
             .containsExactlyInAnyOrder(
-                tuple("001", "아메리카노", SELLING),
-                tuple("002", "카페라떼", SELLING)
+                tuple("A-001", "아메리카노", SELLING),
+                tuple("A-002", "카페라떼", SELLING)
             )
 
     }
 
-    @DisplayName("마지막으로 저장한 상품번호를 조회한다.")
+    @DisplayName("마지막 상품 번호의 상품을 조회한다.")
     @Test
     fun findLatestProduct() {
         //given
-        val targetProductNumber = "003"
-        val product1 = createProduct("001", COFFEE, SELLING, "아메리카노", 3000)
-        val product2 = createProduct("002", BEVERAGE, SELLING, "카페라떼", 5000)
-        val product3 = createProduct(targetProductNumber, DESSERT, SOLD_OUT, "치즈케이크", 7000)
+        val product1 = createProduct("A-001", COFFEE, SELLING, "라떼", 3000)
+        val product2 = createProduct("A-002", COFFEE, SELLING, "더치커피", 3000)
+        val product3 = createProduct("A-003", COFFEE, SELLING, "아메리카노", 3000)
         productRepository.saveAll(listOf(product1, product2, product3))
 
         //when
-        val latestProduct = productRepository.findLatestProduct()
+        val latest = productRepository.findLatestProduct("A")
 
         //then
-        assertThat(latestProduct).isEqualTo("003")
+        assertThat(latest).isEqualTo("A-003")
     }
 
-    @DisplayName("상품이 하나도 없을 때 null을 반환한다.")
+    @DisplayName("상품번호의 prefix로 상품들을 조회한다.")
     @Test
-    fun findLatestProductIsEmpty() {
+    fun findByProductNumberByPrefix() {
+        //given
+        val product1 = createProduct("A-001", COFFEE, SELLING, "아메리카노", 3000)
+        val product2 = createProduct("A-002", COFFEE, SELLING, "더치커피", 3000)
+        val product3 = createProduct("B-001", COFFEE, SELLING, "핫초코", 3000)
+        productRepository.saveAll(listOf(product1, product2, product3))
+
         //when
-        val latestProduct = productRepository.findLatestProduct()
+        val products = productRepository.findByProductNumberByPrefix("A")
 
         //then
-        assertThat(latestProduct).isNull()
+        assertThat(products).hasSize(2)
+            .extracting("productNumber", "name", "price")
+            .containsExactlyInAnyOrder(
+                tuple("A-001", "아메리카노", 3000),
+                tuple("A-002", "더치커피", 3000)
+            )
+
     }
 
     private fun createProduct(
