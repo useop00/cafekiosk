f5df95f0e278386627bd0be3d61f38ffb78b1f39
seop <useop0821@gmail.com>
2025-01-20 08:02:16 +0900

    feat: 재고 엔티티 생성, 낙관적 락으로 관리하고 재고에 대한 메서드 구현

---
+++ src/main/kotlin/hello/kiosk/domain/stock/Stock.kt
@@ -0,0 +1,37 @@
+package hello.kiosk.domain.stock
+
+import hello.kiosk.domain.BaseEntity
+import jakarta.persistence.*
+
+@Entity
+class Stock (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    var productNumber: String,
+    var quantity: Int = 0,
+
+    @Version
+    var version: Long? = null
+
+): BaseEntity(){
+    companion object {
+        fun create(productNumber: String, quantity: Int): Stock {
+            return Stock(
+                productNumber = productNumber,
+                quantity = quantity
+            )
+        }
+    }
+
+    fun isQuantityLessThan(quantity: Int): Boolean {
+        return this.quantity < quantity
+    }
+
+    fun decreaseQuantity(quantity: Int) {
+        if (isQuantityLessThan(quantity)) {
+            throw IllegalArgumentException("재고가 부족합니다.")
+        }
+        this.quantity -= quantity
+    }
+}
