194e83c06f3df5288e707ec148fa8a17fe642504
seop <useop0821@gmail.com>
2025-01-20 08:06:01 +0900

    feat: 상품 번호를 001 -> A-001 이런식으로 변경, 상품 생성할 때 재고가 핑요한 상품인지 확인하고 넣어주는 메서드 생성

--- src/main/kotlin/hello/kiosk/service/product/ProductService.kt
+++ src/main/kotlin/hello/kiosk/service/product/ProductService.kt
@@ -1,19 +1,37 @@
 package hello.kiosk.service.product
 
 import hello.kiosk.domain.product.ProductSellingStatus.Companion.forDisplay
+import hello.kiosk.domain.stock.Stock
 import hello.kiosk.repository.ProductRepository
+import hello.kiosk.repository.StockRepository
 import hello.kiosk.service.product.request.ProductCreateRequest
 import hello.kiosk.service.product.response.ProductResponse
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class ProductService (private val productRepository: ProductRepository) {
+@Transactional(readOnly = true)
+class ProductService (
+    private val productRepository: ProductRepository,
+    private val stockRepository: StockRepository
+) {
 
+    @Transactional
     fun saveProduct(request: ProductCreateRequest): ProductResponse {
-        val nextProductNumber = createNextProductNumber()
+        val prefix = request.type.prefix
+        val nextProductNumber = createNextProductNumber(prefix)
+
         val product = request.toEntity(nextProductNumber)
         val savedProduct = productRepository.save(product)
+
+        stockOfProducts(request, nextProductNumber)
+
         return ProductResponse.of(savedProduct)
+    }
+
+    fun getSellingProductsType(type: String): List<ProductResponse> {
+        val products = productRepository.findByProductNumberByPrefix(type)
+        return products.map { ProductResponse.of(it) }
     }
 
     fun getSellingProducts():List<ProductResponse>{
@@ -21,11 +39,28 @@ class ProductService (private val productRepository: ProductRepository) {
         return products.map { ProductResponse.of(it) }
     }
 
-    private fun createNextProductNumber(): String {
-        val latestProductNumber = productRepository.findLatestProduct() ?: return "001"
-        val latestProductNumberInt: Int = latestProductNumber.toInt()
-        val nextProductNumberInt = latestProductNumberInt + 1
+    private fun stockOfProducts(
+        request: ProductCreateRequest,
+        nextProductNumber: String
+    ) {
+        if (request.type.needStockCheck()) {
+            val initQuantity = request.initialStock ?: 0
+            val stock = Stock.create(nextProductNumber, initQuantity)
+            stockRepository.save(stock)
+        }
+    }
 
-        return String.format("%03d", nextProductNumberInt)
+    private fun createNextProductNumber(prefix: String): String {
+        val latest = productRepository.findLatestProduct(prefix) ?: return "$prefix-001"
+        val split = latest.split("-")
+        if (split.size != 2) {
+            return "$prefix-001"
+        }
+
+        val numberPart = split[1]
+        val nextInt = numberPart.toIntOrNull()?.plus(1) ?: 1
+        val nextStr = String.format("%03d", nextInt)
+
+        return "$prefix-$nextStr"
     }
 }
