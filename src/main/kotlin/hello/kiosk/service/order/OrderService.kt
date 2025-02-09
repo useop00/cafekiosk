13f0c08a37d6e20f422ed7ff706b613f6f81c90b
seop <useop0821@gmail.com>
2025-02-09 20:34:19 +0900

    feat: user에 대한 로직 구현

--- src/main/kotlin/hello/kiosk/service/order/OrderService.kt
+++ src/main/kotlin/hello/kiosk/service/order/OrderService.kt
@@ -2,11 +2,14 @@ package hello.kiosk.service.order
 
 import hello.kiosk.domain.order.Order
 import hello.kiosk.domain.product.Product
+import hello.kiosk.domain.stock.Stock
 import hello.kiosk.exception.NotFoundProduct
+import hello.kiosk.exception.NotFoundUser
 import hello.kiosk.exception.OutOfStock
 import hello.kiosk.repository.OrderRepository
 import hello.kiosk.repository.ProductRepository
 import hello.kiosk.repository.StockRepository
+import hello.kiosk.repository.UserRepository
 import hello.kiosk.service.order.request.OrderRequest
 import hello.kiosk.service.order.response.OrderResponse
 import org.springframework.stereotype.Service
@@ -17,17 +20,20 @@ import java.time.LocalDateTime
 class OrderService(
     private val orderRepository: OrderRepository,
     private val productRepository: ProductRepository,
-    private val stockRepository: StockRepository
+    private val stockRepository: StockRepository,
+    private val userRepository: UserRepository
 ) {
 
     @Transactional
-    fun crateOrder(request: OrderRequest, registeredDateTime: LocalDateTime): OrderResponse {
+    fun createOrder(request: OrderRequest, registeredDateTime: LocalDateTime, username: String): OrderResponse {
         val productNumber = request.productNumber
         val products = findProductsBy(productNumber)
 
+        val user = userRepository.findByUsername(username) ?: throw NotFoundUser("사용자를 찾을 수 없습니다.")
+
         reduceStockBy(products, request)
 
-        val order = Order.create(products, request.productQuantities ,registeredDateTime)
+        val order = Order.create(products, request.productQuantities, registeredDateTime, user)
         val savedOrder = orderRepository.save(order)
 
         return OrderResponse.of(savedOrder)
