ee19bd8ea9989d100bcc9da2a3268413c1c3ec0c
seop <useop0821@gmail.com>
2025-02-09 20:35:01 +0900

    feat: @Lock 적용

--- src/main/kotlin/hello/kiosk/repository/StockRepository.kt
+++ src/main/kotlin/hello/kiosk/repository/StockRepository.kt
@@ -1,10 +1,12 @@
 package hello.kiosk.repository
 
 import hello.kiosk.domain.stock.Stock
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
 
 interface StockRepository : JpaRepository<Stock, Long> {
 
+    @Lock(LockModeType.OPTIMISTIC)
     fun findByProductNumber(productNumber: String): Stock?
-
 }
