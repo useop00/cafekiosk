52c41800540af3e0d2f08fd2b5caee874e58d1bc
seop <useop0821@gmail.com>
2025-02-09 21:48:26 +0900

    feat: 타입별 검색 추가

--- src/main/kotlin/hello/kiosk/controller/product/ProductController.kt
+++ src/main/kotlin/hello/kiosk/controller/product/ProductController.kt
@@ -5,13 +5,14 @@ import hello.kiosk.service.product.ProductService
 import hello.kiosk.service.product.request.ProductCreateRequest
 import hello.kiosk.service.product.response.ProductResponse
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/products")
 class ProductController (
     private val productService: ProductService
 ) {
@@ -23,5 +24,10 @@ class ProductController (
     @GetMapping("/selling")
     fun getSellingProducts(): ApiResponse<List<ProductResponse>> {
         return ApiResponse.ok(productService.getSellingProducts())
+    }
+
+    @GetMapping("/selling/{type}")
+    fun getSellingProductsType(@PathVariable type: String): ApiResponse<List<ProductResponse>> {
+        return ApiResponse.ok(productService.getSellingProductsType(type))
     }
 }
