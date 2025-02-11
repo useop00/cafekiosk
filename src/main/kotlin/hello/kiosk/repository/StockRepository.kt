073044aba54f5ef405ce57691267ac4dbaa612e0
seop <useop0821@gmail.com>
2025-02-12 04:23:15 +0900

    feat: 단일 productNumber 조회추가

--- src/main/kotlin/hello/kiosk/repository/StockRepository.kt
+++ src/main/kotlin/hello/kiosk/repository/StockRepository.kt
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 interface StockRepository : JpaRepository<Stock, Long> {
 
     fun findAllByProductNumberIn(productNumber: List<String>): List<Stock>
+    fun findByProductNumber(productNumber: String): Stock?
 }
