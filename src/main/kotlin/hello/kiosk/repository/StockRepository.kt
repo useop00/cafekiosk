79d6fdf54ebc3e7556b334e952be512e4d590f22
seop <useop0821@gmail.com>
2025-02-12 03:25:06 +0900

    feat: list로 변경

--- src/main/kotlin/hello/kiosk/repository/StockRepository.kt
+++ src/main/kotlin/hello/kiosk/repository/StockRepository.kt
@@ -1,12 +1,9 @@
 package hello.kiosk.repository
 
 import hello.kiosk.domain.stock.Stock
-import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Lock
 
 interface StockRepository : JpaRepository<Stock, Long> {
 
-    @Lock(LockModeType.OPTIMISTIC)
-    fun findByProductNumber(productNumber: String): Stock?
+    fun findAllByProductNumberIn(productNumber: List<String>): List<Stock>
 }
