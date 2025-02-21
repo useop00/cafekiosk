995323650766e6c4f0a848907fcfe49e90cd6cf3
seop <useop0821@gmail.com>
2025-02-22 00:27:26 +0900

    feat: swagger 어노테이션 추가

--- src/main/kotlin/hello/kiosk/controller/product/ProductController.kt
+++ src/main/kotlin/hello/kiosk/controller/product/ProductController.kt
@@ -3,30 +3,32 @@ package hello.kiosk.controller.product
 import hello.kiosk.controller.ApiResponse
 import hello.kiosk.controller.product.request.ProductCreateRequest
 import hello.kiosk.service.product.ProductService
-import hello.kiosk.service.product.request.ProductCreateServiceRequest
 import hello.kiosk.service.product.response.ProductResponse
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/products")
+@Tag(name = "Product", description = "상품 API")
 class ProductController (
     private val productService: ProductService
 ) {
+
+    @Operation(summary = "상품 등록", description = "상품을 등록합니다.")
     @PostMapping
-    fun createProduct(@RequestBody request: ProductCreateRequest): ApiResponse<ProductResponse> {
+    fun createProduct(@Valid @RequestBody request: ProductCreateRequest): ApiResponse<ProductResponse> {
         return ApiResponse.ok(productService.saveProduct(request.toServiceRequest()))
     }
 
+    @Operation(summary = "판매중인 상품 조회", description = "판매중인 상품들을 필터링하여 목록으로 반환합니다.")
     @GetMapping("/selling")
     fun getSellingProducts(): ApiResponse<List<ProductResponse>> {
         return ApiResponse.ok(productService.getSellingProducts())
     }
 
+    @Operation(summary = "판매중인 상품 중 타입별로 조회", description = "판매중인 상품들을 필터링하고 타입별로 핕터링하여 목록으로 반환합니다.")
     @GetMapping("/selling/{type}")
     fun getSellingProductsType(@PathVariable type: String): ApiResponse<List<ProductResponse>> {
         return ApiResponse.ok(productService.getSellingProductsType(type))
--- src/main/kotlin/hello/kiosk/controller/stock/StockController.kt
+++ src/main/kotlin/hello/kiosk/controller/stock/StockController.kt
@@ -3,25 +3,26 @@ package hello.kiosk.controller.stock
 import hello.kiosk.controller.ApiResponse
 import hello.kiosk.controller.stock.request.StockCreateRequest
 import hello.kiosk.service.stock.StockService
-import hello.kiosk.service.stock.request.StockCreateServiceRequest
 import hello.kiosk.service.stock.response.StockResponse
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/stocks")
+@Tag(name = "Stock", description = "재고 API")
 class StockController (
     private val stockService: StockService
 ) {
 
+    @Operation(summary = "재고 등록", description = "상품 타입이 BOTTLE, DESSERT일 경우 재고를 등록합니다.")
     @PostMapping
-    fun createStock(@RequestBody request: StockCreateRequest): ApiResponse<StockResponse> {
+    fun createStock(@Valid @RequestBody request: StockCreateRequest): ApiResponse<StockResponse> {
         return ApiResponse.ok(stockService.upsertStock(request.toServiceRequest()))
     }
 
+    @Operation(summary = "재고 조회", description = "재고를 조회합니다.")
     @GetMapping
     fun getStocks(): ApiResponse<List<StockResponse>> {
         return ApiResponse.ok(stockService.getAllStocks())
