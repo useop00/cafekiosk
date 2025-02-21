32d41e9ccaf78fb76e6cd9e0544e5d0c0009da12
seop <useop0821@gmail.com>
2025-02-22 00:33:12 +0900

    feat: 비관적 락 설정

--- src/main/kotlin/hello/kiosk/repository/StockRepository.kt
+++ src/main/kotlin/hello/kiosk/repository/StockRepository.kt
@@ -1,10 +1,14 @@
 package hello.kiosk.repository
 
 import hello.kiosk.domain.stock.Stock
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
 
 interface StockRepository : JpaRepository<Stock, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     fun findAllByProductNumberIn(productNumber: List<String>): List<Stock>
+
     fun findByProductNumber(productNumber: String): Stock?
 }
