dc831dae8ab34d4b44039c757332ce73be11a8d5
seop <useop0821@gmail.com>
2025-02-22 00:31:06 +0900

    test: 통합테스트 픽스처 생성

---
+++ src/test/kotlin/hello/kiosk/IntegrationTestSupport.kt
@@ -0,0 +1,11 @@
+package hello.kiosk
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.transaction.annotation.Transactional
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+abstract class IntegrationTestSupport {
+}
